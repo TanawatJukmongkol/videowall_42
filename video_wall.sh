a31e641d3a6f0d2744cda9a7016caf4f5b626f64
Tanawat Jukmongkol <tjukmong@z2t16c1.42bangkok.com>
2025-06-09 22:31:40 +0700

    bruh

--- video_wall.sh
+++ video_wall.sh
@@ -1,4 +1,7 @@
 # No need for systems that has the required dependencies
-export LD_LIBRARY_PATH=lib/x86_64-linux-gnu/
 
-./xwinwrap -fs -fdt -ni -b -nf -un -- ./mpv -wid WID --loop --no-audio ./wallpaper.webm & disown
+export PWD=/home/tjukmong/Project/xwinwrap
+export LD_LIBRARY_PATH=$PWD/lib/x86_64-linux-gnu/
+$PWD/xwinwrap -fs -fdt -ni -b -nf -un -- $PWD/mpv -wid WID --loop --no-audio $PWD/wallpaper.webm
+#./xwinwrap -fs -fdt -ni -b -nf -un -- ./mpv -wid WID --loop --no-audio ./rick-roll.webm
+
