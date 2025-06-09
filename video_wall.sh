cbf61c5933579ace63aaa4392b69a9d64d02e180
Tanawat Jukmongkol <tjukmong@z2t16c1.42bangkok.com>
2025-06-09 22:45:52 +0700

    fixed autostart

--- video_wall.sh
+++ video_wall.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 # No need for systems that has the required dependencies
 
-export PWD=/home/tjukmong/Project/xwinwrap
+export PWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export LD_LIBRARY_PATH=$PWD/lib/x86_64-linux-gnu/
 $PWD/xwinwrap -fs -fdt -ni -b -nf -un -- $PWD/mpv -wid WID --loop --no-audio $PWD/wallpaper.webm
 #./xwinwrap -fs -fdt -ni -b -nf -un -- ./mpv -wid WID --loop --no-audio ./rick-roll.webm
